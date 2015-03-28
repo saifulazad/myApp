2cf1bde9f1add9439fc99c6e319ba18bd335b2bd
unknown <sai108710@gmail.com>
2015-03-28 21:54:35 +0600

    Html Page layout change

--- app/user_info.py
+++ app/user_info.py
@@ -1,35 +1,32 @@
 import redis
 
 
-
 class Userinfo:
+    def __init__(self, email):
+        self.db = redis.Redis(host='localhost', port=6379, db=0)
 
-	def __init__(self , email):
-		self.db = redis.Redis(host='localhost', port=6379, db=0)
-		
-		self.email = self.db.get(email)
-		
-		
-			
-	def is_matched(self , email,  password):
-		
-		if self.email == password:
-			return True
-		
-		return False
-		
-        def is_authenticated(self):
-                return True
+        self.email = self.db.get(email)
 
-        def is_active(self):
-                return True
 
-        def is_anonymous(self):
-                return False
+    def is_matched(self, email, password):
+        if self.email == password:
+            return True
 
-        def get_id(self):
-                return self.email
-        def __repr__(self):
-                return '<User %r>' % (self.email)	
+        return False
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.email
+
+    def __repr__(self):
+        return '<User %r>' % (self.email)
 		
 		
