cbb7a54d6ab8e7a576033f962e5dbeafc93a74c4
YOUR NAME <sai108710@gmail.com>
2015-04-02 12:39:42 +0600

    ok

--- app/views.py
+++ app/views.py
@@ -3,13 +3,13 @@ from flask import render_template
 
 import os
 from flask import Flask, url_for, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from wtforms import form, fields, validators
-import flask_admin as admin
-import flask_login as login
-from flask_admin.contrib import sqla
-from flask_admin import helpers, expose
-from werkzeug.security import generate_password_hash, check_password_hash
+# from flask_sqlalchemy import SQLAlchemy
+# from wtforms import form, fields, validators
+# import flask_admin as admin
+# import flask_login as login
+# from flask_admin.contrib import sqla
+# from flask_admin import helpers, expose
+# from werkzeug.security import generate_password_hash, check_password_hash
 from  models import *
 
 
