508c69b6ef1de9caefe011e1fac5f209dcb07df8
saifulazad <sai108710@gmail.com>
2015-05-27 23:25:28 +0600

    just comment

--- app/models.py
+++ app/models.py
@@ -1,18 +1,18 @@
 from app import db
 
-class Product(db.Model):
-
-    id = db.Column(db.Integer , primary_key=True)
-
-    product_name = db.Column(db.String(120), index=True , unique=True,nullable = False)
-
-    price = db.Column(db.Integer,nullable = False)
-
-    amount = db.Column(db.Integer, nullable = False)
-
-
-    def __repr__(self):
-        return '<product_name %r price %r amount  %r>' % (self.product_name, self.price,self.amount)
+# class Product(db.Model):
+#
+#     id = db.Column(db.Integer , primary_key=True)
+#
+#     product_name = db.Column(db.String(120), index=True , unique=True,nullable = False)
+#
+#     price = db.Column(db.Integer,nullable = False)
+#
+#     amount = db.Column(db.Integer, nullable = False)
+#
+#
+#     def __repr__(self):
+#         return '<product_name %r price %r amount  %r>' % (self.product_name, self.price,self.amount)
 
 #create Question model
 class Questiontable (db.Model):
--- app/views.py
+++ app/views.py
@@ -39,7 +39,7 @@ def csrf_error(reason):
 def index():
     return render_template("base.html")
 
-@app.route('/profile', methods=['GET', 'POST'])
+@app.route('/profile', methods=['GET'])
 def profile():
 #    form = UserProfile(request.form)
 #   generating random numbers
@@ -152,38 +152,15 @@ def login():
 #     # str(1)
 #
 #     return render_template('question.html',question=question )
-# @app.route('/questions/<questionsID>')
-# def user(questionsID):
-#     product = Product.query.filter_by(product_name=questionsID).first()
-#     return render_template('info_of_product.html', prduc=product)
+@app.route('/questions/<questionsID>')
+def user(questionsID):
+
+    question = Questiontable.query.filter_by(questionID=questionsID).first()
+    return render_template('question.html', question=question)
 
 
 
-#
-# def register():
-#     form = RegistrationForm(request.form)
-#
-#     if request.method == 'POST' and form.validate_on_submit():
-#
-#
-#         print form.catalogue_name.data
-#         print form.price.data
-#         print form.dressid.data
-#         print form.catalogue_company.data
-#
-#         new_product = Product_details(dressid= form.dressid.data, catalogue =form.catalogue.data,
-#                 catalogue_name=form.catalogue_name.data,price= form.price.data,
-#                 quantity=form.quantity.data, catalogue_company=form.catalogue_company.data,
-#                 color=form.color.data,category= form.category.data
-#                 )
-#
-#         db.session.add(new_product)
-#         db.session.commit()
-#         # flash('Thanks for registering')
-#         # return redirect(url_for('login'))
-#
-#
-#     return render_template('reg.html', form=form)
+
 
 
 
